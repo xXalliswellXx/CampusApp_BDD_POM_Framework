2ee9488bc829e548edd6fd75a4ac96840718d8e1
alliswell <koray.akcay55@gmail.com>
2022-09-22 05:23:24 +0300

    NavigationBarElements and DialogContentElements

---
+++ src/test/java/POM/DialogContentElements.java
@@ -0,0 +1,20 @@
+package POM;
+
+import org.openqa.selenium.support.PageFactory;
+
+public class DialogContentElements extends BasePOM {
+
+    // ############# Constructor  #############
+
+    public DialogContentElements() {
+        PageFactory.initElements(driver, this);
+    }
+
+    // ############# Web Elements  #############
+
+
+    // ############# Functions  #############
+
+
+
+}
---
+++ src/test/java/POM/NavigationBarElements.java
@@ -0,0 +1,21 @@
+package POM;
+
+import org.openqa.selenium.support.PageFactory;
+
+public class NavigationBarElements extends BasePOM {
+
+    // ############# Constructor  #############
+
+    public NavigationBarElements() {
+        PageFactory.initElements(driver, this);
+    }
+
+
+    // ############# Web Elements  #############
+
+
+    // ############# Functions  #############
+
+
+
+}
