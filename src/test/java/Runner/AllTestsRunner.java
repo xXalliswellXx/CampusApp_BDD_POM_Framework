2fdf6e1549fcca3a7acb59cd562cb0567a79dac2
alliswell <koray.akcay55@gmail.com>
2022-09-24 16:28:37 +0300

    Runner Update

---
+++ src/test/java/Runner/AllTestsRunner.java
@@ -0,0 +1,12 @@
+package Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/main/resources/FeatureFiles"},
+        glue = {"StepDefinitions"},
+        plugin = {"html:target/report/RegressionTest.html"}
+)
+
+public class AllTestsRunner extends AbstractTestNGCucumberTests {}
